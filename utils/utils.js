61f3010f0581d61023f13f5509ee3297317e4cf7
jefedeltejas <jeff.jackson@dashiell.com>
2017-03-29 14:34:31 -0500

    modified test

--- utils/utils.js
+++ utils/utils.js
@@ -1,1 +1,1 @@
-module.exports.add = (a, b) => a + b;
+module.exports.add = (a, b) => a + b + 100;
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -2,5 +2,8 @@ const utils = require('./utils');
 
 it('should add 2 nums', () => {
   var res = utils.add(33, 4);
-  throw new Error('PC load letter');
+
+  if (res !== 37) {
+    throw new Error(`Expected 37, but got ${res}.`)
+  }
 });
