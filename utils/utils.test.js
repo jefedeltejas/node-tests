d42f3a5245b5d757f2efdde0d1386a0a0feeaf0c
jefedeltejas <jeff.jackson@dashiell.com>
2017-03-29 16:50:21 -0500

    added expect module and test-watch script

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,11 +1,13 @@
-const utils = require('./utils');
+const expect = require('expect'),
+      utils = require('./utils');
 
 it('should add 2 nums', () => {
   var res = utils.add(33, 4);
 
-  if (res !== 37) {
-    throw new Error(`Expected 37, but got ${res}.`)
-  }
+  expect(res).toBe(37);)
+  // if (res !== 37) {
+  //   throw new Error(`Expected 37, but got ${res}.`)
+  // }
 });
 
 it('should square some num', () => {
