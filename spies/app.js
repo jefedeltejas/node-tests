e19a213c451ed991af756a7d5a6f5e3fc49abce3
jefedeltejas <jeff.jackson@dashiell.com>
2017-03-30 17:55:28 -0500

    finished spy tests

--- spies/app.js
+++ spies/app.js
@@ -2,9 +2,6 @@ var db = require('./db.js');
 
 module.exports.handleSignup = (email, password) => {
   // Check if email already exists
-  db.saveUser({
-    email,
-    password
-  });
+  db.saveUser({email, password});
   // Send the welcome email
 };
--- spies/app.test.js
+++ spies/app.test.js
@@ -1,11 +1,26 @@
-const expect = require('expect');
+const expect = require('expect'),
+      rewire = require('rewire');
+
+var app = rewire('./app');
 
 describe('App', () => {
+  var db = {
+    saveUser: expect.createSpy()
+  };
+  app.__set__('db', db);
 
   it('should call the spy correctly', () => {
     var spy = expect.createSpy();
     spy();
     expect(spy).toHaveBeenCalled();
+  });
+
+  it('should call saveUser with user object', () => {
+    var email = 'rick.ross@boss.com';
+    var password = 'thirsty123';
+
+    app.handleSignup(email, password);
+    expect(db.saveUser).toHaveBeenCalledWith({email, password});
   });
 
 });
